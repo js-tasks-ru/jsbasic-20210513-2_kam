020d3caa4a5d4e982f81988950561cec644cb13c
Kam <kam06@mail.ru>
2021-05-28 12:43:17 +0300

    Master. Anouther edited task 2-1.

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,7 +1,7 @@
 function sumSalary(salaries) {
   let sum = 0;
   for (let key in salaries) {
-    if (isFinite(salaries.key)) {
+    if (isFinite(salaries[key])) {
       sum += salaries[key];
     }
   }
